59f62b06f98f58e843687d8c5206330a2bf46665
Anusha-J-5 <81564900+Anusha-J-5@users.noreply.github.com>
2021-04-10 11:31:33 +0530

    Create modify_records.c

---
+++ 3_Implementation/modify_records.c
@@ -0,0 +1,30 @@
+void modifyrecords()
+{
+	FILE *f;
+	char phonenumber[20];
+	long int size=sizeof(s);
+	if((f=fopen("E:/file.txt","rb+"))==NULL)
+		exit(0);
+	system("cls");
+	printf("Enter phone number of the subscriber to modify:");
+	scanf("%[^\n]",phonenumber);
+	fflush(stdin);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.phonenumber,phonenumber)==0)
+		{
+			system("cls");
+			printf("\n Enter phone number:");
+			scanf("%s",&s.phonenumber);
+			printf("\n Enter name: ");
+			fflush(stdin);
+			scanf("%[^\n]",&s.name);
+			printf("\n Enter amount: ");
+			scanf("%f",&s.amount);
+			fseek(f,-size,SEEK_CUR);
+			fwrite(&s,sizeof(s),1,f);
+			break;
+		}
+	}
+	fclose(f);
+}
