8c2eb9b4f377ac1ca9f8f99195cf04ec87be676f
Anusha-J-5 <81564900+Anusha-J-5@users.noreply.github.com>
2021-04-12 10:07:02 +0530

    Create fun.h

---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,9 @@
+#ifndef __fUN_H__
+	#define __FUN_H__
+
+	void addrecords();
+        void deleterecords();
+        void listrecords();
+        void searchrecords();
+        void modifyrecords();
+	#endif
