0341fc1e5790144537075c810a1362cf87948d81
Anusha-J-5 <81564900+Anusha-J-5@users.noreply.github.com>
2021-04-12 10:06:10 +0530

    Delete add_record.h

--- 3_Implementation/inc/add_record.h
+++
@@ -1,9 +0,0 @@
-#ifndef __fUN_H__
-	#define __FUN_H__
-
-	void addrecords();
-        void deleterecords();
-        void listrecords();
-        void searchrecords();
-        void modifyrecords();
-	#endif
