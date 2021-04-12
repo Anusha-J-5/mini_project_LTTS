559d80c640fb7043b3f8da948bb9a6f2221c8296
Anusha-J-5 <81564900+Anusha-J-5@users.noreply.github.com>
2021-04-12 10:05:26 +0530

    Update add_record.h

--- 3_Implementation/inc/add_record.h
+++ 3_Implementation/inc/add_record.h
@@ -1,5 +1,9 @@
-#ifndef __ADDREC_H__
-	#define __ADDREC_H__
+#ifndef __fUN_H__
+	#define __FUN_H__
 
 	void addrecords();
+        void deleterecords();
+        void listrecords();
+        void searchrecords();
+        void modifyrecords();
 	#endif
