8fc6c768fbb8baeed740e4b337860eeff8bd3816
Anusha-J-5 <81564900+Anusha-J-5@users.noreply.github.com>
2021-04-14 20:41:46 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -1,1 +1,70 @@
+#include "unity.h"
+#include <fun.h>
+
+/* Modify these two lines according to the project */
+#include <fun.h>
+#define PROJECT_NAME    "Contact_Management"
+
+/* Prototypes for all the test functions */
+  void addrecords();
+	void deleterecords();
+	void listrecords();
+	void searchrecords();
+	void modifyrecords();
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_add);
+  RUN_TEST(test_delete);
+  RUN_TEST(test_list);
+  RUN_TEST(test_search);
+  RUN_TEST(test_modify);
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void test_add(void) {
+  TEST_ASSERT_EQUAL(, addrecords());
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(, addrecords());
+}
+
+void test_delete(void) {
+  TEST_ASSERT_EQUAL(, test_delete());
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(, test_delete());
+}
+
+void test_list(void) {
+  TEST_ASSERT_EQUAL(, test_list());
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(, test_list());
+}
+
+void test_search(void) {
+  TEST_ASSERT_EQUAL(,test_search());
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(,test_search());
+}
+void test_modify(void) {
+  TEST_ASSERT_EQUAL(,test_modify());
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(,test_modify());
+}
 
