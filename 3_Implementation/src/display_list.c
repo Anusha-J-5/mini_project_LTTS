b97eb2131028bc5c970a79af1b320d9c3e884e35
Anusha-J-5 <81564900+Anusha-J-5@users.noreply.github.com>
2021-04-14 15:42:02 +0530

    Update display_list.c

--- 3_Implementation/src/display_list.c
+++ 3_Implementation/src/display_list.c
@@ -5,12 +5,12 @@ void listrecords()
 	if((f=fopen("https://github.com/Anusha-J-5/mini_project_LTTS/blob/main/3_Implementation/temp.txt","rb"))==NULL)
 		exit(0);
 	system("cls");
-	printf("Phone Number\t\tUser Name\t\t\tAmount\n");
+	printf("Phone Number\t\tUser Name\t\t\tAge\n");
 	for(i=0;i<79;i++)
 		printf("-");
 	while(fread(&s,sizeof(s),1,f)==1)
 	{
-		printf("\n%-10s\t\t%-20s\t\tRs. %.2f /-",s.phonenumber,s.name,s.amount);
+		printf("\n%-10s\t\t%-20s\t\t %.2f /-",s.phonenumber,s.name,s.age);
 	}
 	printf("\n");
 	for(i=0;i<79;i++)
